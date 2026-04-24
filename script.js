d4c6a6838867ef76959f7d0b6c91cd115c3f6f26
lakkampallysamyuktha <87126592+lakkampallysamyuktha@users.noreply.github.com>
2026-04-24 20:44:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -111,24 +111,4 @@ window.addEventListener("scroll", () => {
   });
 });
 // Services Page
-function toggleFAQ(el) {
-  const items = document.querySelectorAll(".faq-item");
 
-  items.forEach(item => {
-    if (item !== el.parentElement) {
-      item.classList.remove("active");
-      item.querySelector("p").style.maxHeight = null;
-    }
-  });
-
-  const item = el.parentElement;
-  const p = el.nextElementSibling;
-
-  item.classList.toggle("active");
-
-  if (p.style.maxHeight) {
-    p.style.maxHeight = null;
-  } else {
-    p.style.maxHeight = p.scrollHeight + "px";
-  }
-}
