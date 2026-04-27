9f0f2506d636f4c4f32c30796a3611399ab30b6a
lakkampallysamyuktha <87126592+lakkampallysamyuktha@users.noreply.github.com>
2026-04-27 12:29:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -180,3 +180,24 @@ function logout() {
 
   window.addEventListener("scroll", reveal);
   reveal();
+
+
+// ===== FAQ TOGGLE =====
+const faqItems = document.querySelectorAll(".faq-item");
+
+faqItems.forEach(item => {
+  const question = item.querySelector(".faq-question");
+
+  question.addEventListener("click", () => {
+
+    // close all other items
+    faqItems.forEach(i => {
+      if (i !== item) {
+        i.classList.remove("active");
+      }
+    });
+
+    // toggle current
+    item.classList.toggle("active");
+  });
+});
